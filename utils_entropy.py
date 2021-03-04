d07eaeb1ceef12a8f3da453fe18a92a60dbb1e41
FabriFalasca <fabrifalasca@gmail.com>
2021-03-04 13:23:13 -0500

    Update utils_entropy.py

--- utils_entropy.py
+++ utils_entropy.py
@@ -1,7 +1,6 @@
 # Functions for computations of
     # (1) Entropy of a time series
-    # (4) Maximum entropy principle S_max: this is the entropy
-    #     quantifier we want to use
+    # (4) Maximum entropy principle S_max
     # (5) Compute S_max for all grid points in a spatiotemporal grid
 
 # Fabri Falasca
@@ -26,51 +25,34 @@ def recurrenceEntropy(timeSeries, eps,n,sampleSize):
     dimT = len(timeSeries)
     # Initialize microstates
     micro = np.array([], float)
-    
-    # If there is even just 1 nan, return a nan
-    if np.isnan(np.sum(timeSeries)):
 
-        normalizedEntropy = np.nan
-    
-    # If all entries are zeros give a zero back
-    elif np.count_nonzero(timeSeries) == 0:
-        
-        normalizedEntropy = 0
-    
-    # If the time series have std = 0 give 0
-    elif np.std(timeSeries) == 0:
-
-        normalizedEntropy = 0
-
-    else:
-
-        for i in range(sampleSize):
-            # Bounds
-            a = random.randint(0, dimT-n)
-            b = random.randint(0, dimT-n)
-            # Consider 4 consecutive times  t(n),t(n+1),t(n+2),t(n+3)
-            # Choose the randomly. Then do the same t(n),t(n+1),t(n+2),t(n+3)
-            # All distances between all points in the time series
-            d = cdist(timeSeries[a:a+n,None],timeSeries[b:b+n,None],metric='euclidean')
-            # Heaviside function
-            d[d<=eps]=1
-            #d[(d>eps)&(d!=1)]=0
-            d[d!=1]=0
-            micro = np.append(micro, d)
-            # reshape the microstates
-        microstates = micro.reshape((sampleSize,n,n))
-        occurenceList = np.unique(microstates,axis=0, return_counts=1)[1]
-        # From a list of occurences I compute the probability of
-        probabilityList = occurenceList/float(sampleSize)
-        # given the probability list we can define a recurrence entropy based on microstates in RP
-        # Step (1) remove all zeros
-        noZerosProbabilityList =  probabilityList[probabilityList>0]
-        # Step (2) compute the Shannon Entropy of the Probability of occurence of those microstates
-        entropy = - np.sum(noZerosProbabilityList * np.log(noZerosProbabilityList))
-        # Max Entropy (found analytically)
-        maxEntropy = np.log(sampleSize)
-        # Normalized entropy
-        normalizedEntropy = entropy/maxEntropy
+    for i in range(sampleSize):
+        # Bounds
+        a = random.randint(0, dimT-n)
+        b = random.randint(0, dimT-n)
+        # Consider n consecutive times  t(n),t(n+1),t(n+2),t(n+3)
+        # Choose the randomly. Then do the same t(n),t(n+1),t(n+2),t(n+3)
+        # All distances between all points in the time series
+        d = cdist(timeSeries[a:a+n,None],timeSeries[b:b+n,None],metric='euclidean')
+        # Heaviside function
+        d[d<=eps]=1
+        #d[(d>eps)&(d!=1)]=0
+        d[d!=1]=0
+        micro = np.append(micro, d)
+        # reshape the microsta t e s
+    microstates = micro.reshape((sampleSize,n,n))
+    occurenceList = np.unique(microstates,axis=0, return_counts=1)[1]
+    # From a list of occurences I compute the probability of
+    probabilityList = occurenceList/float(sampleSize)
+    # given the probability list we can define a recurrence entropy based on microstates in RP
+    # Step (1) remove all zeros
+    noZerosProbabilityList =  probabilityList[probabilityList>0]
+    # Step (2) compute the Shannon Entropy of the Probability of occurence of those microstates
+    entropy = - np.sum(noZerosProbabilityList * np.log(noZerosProbabilityList))
+    # Max Entropy (found analytically)
+    maxEntropy = np.log(sampleSize)
+    # Normalized entropy
+    normalizedEntropy = entropy/maxEntropy
 
     return normalizedEntropy
 
@@ -83,21 +65,38 @@ def recurrenceEntropy(timeSeries, eps,n,sampleSize):
 #           all tests showed convergence at sampleSize = 10000
 def s_max(timeSeries, n, sampleSize_h, sampleSize):
 
-    # Entropy for the heuristic
-    entropy_ts_h = np.array([],float)
-    # epsilons to test
-    # We compute epsilons from 0 to 150% of the standard deviation of the time series
-    epsilons = np.arange(0,1.55,0.05) * np.std(timeSeries)
+    # If there is even just 1 nan, return a nan
+    if np.isnan(np.sum(timeSeries)):
 
-    for i in np.arange(len(epsilons)):
-        entropy_ts_h = np.append(entropy_ts_h,recurrenceEntropy(timeSeries, epsilons[i],n,sampleSize_h))
+        final_entropy = np.nan
 
-    # Find the epsilon that maximize the entropy
-    eps = epsilons[entropy_ts_h == np.max(entropy_ts_h)][0]
-    #eps = epsilons[entropy_ts_h == np.max(entropy_ts_h)]
+    # If all entries are zeros give a zero back
+    elif np.count_nonzero(timeSeries) == 0:
 
-    # Use this epsilon to compute the final entropy
-    final_entropy = recurrenceEntropy(timeSeries, eps,n,sampleSize)
+        final_entropy = 0
+
+    # If the time series have std = 0 give 0
+    elif np.std(timeSeries) == 0:
+
+        final_entropy = 0
+
+    else:
+
+        # Entropy for the heuristic
+        entropy_ts_h = np.array([],float)
+        # epsilons to test
+        # We compute epsilons from 0 to 150% of the standard deviation of the time series
+        epsilons = np.arange(0,1.55,0.05) * np.std(timeSeries)
+
+        for i in np.arange(len(epsilons)):
+            entropy_ts_h = np.append(entropy_ts_h,recurrenceEntropy(timeSeries, epsilons[i],n,sampleSize_h))
+
+        # Find the epsilon that maximize the entropy
+        eps = epsilons[entropy_ts_h == np.max(entropy_ts_h)]
+        #eps = epsilons[entropy_ts_h == np.max(entropy_ts_h)]
+
+        # Use this epsilon to compute the final entropy
+        final_entropy = recurrenceEntropy(timeSeries, eps[0],n,sampleSize)
 
     return final_entropy
 
@@ -125,11 +124,7 @@ def recurrenceEntropyField(field,n,sampleSize_h, sampleSize):
 
     for i in range(0,dimLat):
         for j in range(0,dimLon):
-            # Check if is a masked value
-            if np.isnan(field[0,i,j]):
-                entropyField[i,j] = field[0,i,j] # if it is mask just return the mask
-            else:
-                entropyField[i,j] = s_max(field[:,i,j], n, sampleSize_h, sampleSize)
+            entropyField[i,j] = s_max(field[:,i,j], n, sampleSize_h, sampleSize)
 
 
     return entropyField
